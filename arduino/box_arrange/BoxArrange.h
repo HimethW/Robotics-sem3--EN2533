038c12b9b74902f4c570ece85cb9821dedc9ea5b
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-09-13 23:19:58 +0530

    Created BoxArrange.h

---
+++ arduino/box_arrange/BoxArrange.h
@@ -0,0 +1,17 @@
+/*
+  BoxArrange.h
+  EN2533 - Robot Design and Competition
+  Pulasthi Udugamasooriya, 13 September, 2024
+*/
+
+#ifndef BoxArrange_h
+#define BoxArrange_h
+
+#include "Arduino.h"
+#include "Robot.h"
+
+void boxToTemp(Robot *robot, byte startPos, byte endPos);
+void tempToBox(Robot *robot, byte startPos, byte endPos);
+void boxToBox(Robot *robot, byte startPos, byte endPos);
+
+#endif
