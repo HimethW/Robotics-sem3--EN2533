7fccea0f8c0b81ca8b158c9efcb668c545c3273a
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-09-14 23:06:08 +0530

    Create Robot.cpp

---
+++ arduino/Robot/Robot.cpp
@@ -0,0 +1,89 @@
+/*
+  Robot.cpp
+  EN2533 - Robot Design and Competition
+  Pulasthi Udugamasooriya, 14 September, 2024
+*/
+
+#include "Arduino.h"
+#include "Robot.h"
+#include "Wheels.h"
+
+Robot::Robot() {
+  _speed = 0;
+}
+
+void Robot::attachWheels(Wheel_t *leftWheels, byte numLeftWheels, Wheel_t *rightWheels, byte numRightWheels) {
+  _leftWheels = (Wheel_t*)malloc(sizeof(Wheel_t) * numLeftWheels);
+  _numLeftWheels = numLeftWheels;
+  for (byte i = 0; i < numLeftWheels; i++) {
+    _leftWheels[i] = Wheel(leftWheels[i].controlPinA, leftWheels[i].controlPinB, leftWheels[i].speedPin);
+  }
+  
+  _rightWheels = (Wheel_t*)malloc(sizeof(Wheel_t) * numRightWheels);
+  _numRightWheels = numRightWheels;
+  for (byte i = 0; i < numRightWheels; i++) {
+    _rightWheels[i] = Wheel(rightWheels[i].controlPinA, rightWheels[i].controlPinB, rightWheels[i].speedPin);
+  }
+}
+
+void Robot::setLeftSpeed(float speed) {
+  for (byte i = 0; i < numLeftWheels; i++) {
+    _leftWheels[i].setSpeed(speed);
+  }
+}
+
+void Robot::setRightSpeed(float speed) {
+  for (byte i = 0; i < numRightWheels; i++) {
+    _rightWheels[i].setSpeed(speed);
+  }
+}
+
+void Robot::setBaseSpeed(float speed) {
+  _speed = speed;
+  setRightSpeed(speed);
+  setLeftSpeed(speed);
+}
+
+void Robot::drive(byte direction) {
+  if (direction == FORWARD) {
+    for (byte i = 0; i < numLeftWheels; i++) {
+      _leftWheels[i].forward();
+    }
+    for (byte i = 0; i < numRightWheels; i++) {
+      _rightWheels[i].forward();
+    }
+  } else if (direction == BACKWARD) {
+    for (byte i = 0; i < numLeftWheels; i++) {
+      _leftWheels[i].backward();
+    }
+    for (byte i = 0; i < numRightWheels; i++) {
+      _rightWheels[i].backward();
+    }
+  }
+}
+
+void Robot::brake() {
+  for (byte i = 0; i < numLeftWheels; i++) {
+    _leftWheels[i].brake();
+  }
+  for (byte i = 0; i < numRightWheels; i++) {
+    _rightWheels[i].brake();
+  }
+}
+
+void Robot::driveDistance(byte direction, int distance) {
+  drive(direction);
+  bool condition = true;
+  while (condition) {
+    condition = false; // keep reading encoders here an updating condition to see if the distance has been moved
+  }
+  brake();
+}
+
+void Robot::turn(int radius, int angle) {
+  // don't know how to do this yet. maybe keep turning, reading the compass, to make sure we've turned through the angle
+}
+
+void Robot::attachGripper(Gripper *gripper) {
+  _gripper = gripper;
+}
