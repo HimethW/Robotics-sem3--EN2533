e74fb42f7456d4dd03842e25145850dc992d2de0
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-09-14 12:21:12 +0530

    Update LineFollower.h

--- arduino/LineFollower/LineFollower.h
+++ arduino/LineFollower/LineFollower.h
@@ -1,1 +1,17 @@
+/*
+  LineFollower.h
+  EN2533 - Robot Design and Competition
+  Pulasthi Udugamasooriya, 14 September, 2024
+*/
 
+#ifndef LineFollower_h
+#define LineFollower_h
+
+#include "Arduino.h"
+#include "Robot.h"
+
+void readIRSensors(float *readings); // change to int* if digital sensors are used
+float calculateError(float *readings);
+float getPID(float error, float *errorIntegral, float *errorDerivative);
+
+#endif
