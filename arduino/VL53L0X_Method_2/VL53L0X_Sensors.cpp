f61c278b3f9bba09dc34ee93ca97ef25791e88c2
Pravindu Goonetilleke <pravindugoonetilleke@gmail.com>
2024-09-23 17:01:41 +0530

    Implementation of method 2 for height detection added. Header and Source files for TOF sensor functionality added.

---
+++ arduino/VL53L0X_Method_2/VL53L0X_Sensors.cpp
@@ -0,0 +1,69 @@
+#include "VL53L0X_Sensors.h"
+
+// Create objects for the sensors
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+
+// Measurement data structures
+VL53L0X_RangingMeasurementData_t measure1;
+VL53L0X_RangingMeasurementData_t measure2;
+
+// Sensor initialization function
+void initSensors() {
+    pinMode(SHT_LOX1, OUTPUT);
+    pinMode(SHT_LOX2, OUTPUT);
+
+    digitalWrite(SHT_LOX1, LOW);
+    digitalWrite(SHT_LOX2, LOW);
+    delay(10);
+
+    digitalWrite(SHT_LOX1, HIGH);
+    digitalWrite(SHT_LOX2, HIGH);
+    delay(10);
+
+    digitalWrite(SHT_LOX1, HIGH);
+    digitalWrite(SHT_LOX2, LOW);
+
+    if (!lox1.begin(LOX1_ADDRESS)) {
+        Serial.println(F("Failed to boot first VL53L0X"));
+        while (1);
+    }
+
+    delay(10);
+
+    digitalWrite(SHT_LOX2, HIGH);
+    delay(10);
+
+    if (!lox2.begin(LOX2_ADDRESS)) {
+        Serial.println(F("Failed to boot second VL53L0X"));
+        while (1);
+    }
+}
+
+int read_sensor1() {
+  
+  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
+ 
+
+  // print sensor one reading
+  if(measure1.RangeStatus != 4) {     // if not out of range
+    int sensor1 = measure1.RangeMilliMeter;    
+    return sensor1;   
+  } else {
+    //Serial.println("1 Out of range");
+  }
+}
+
+int read_sensor2() {
+
+  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
+
+  // print sensor two reading
+  if(measure2.RangeStatus != 4) {
+    int sensor2 = measure2.RangeMilliMeter;
+    return sensor2;
+  } else {
+    //Serial.println("2 Out of range");
+  }
+  
+}
