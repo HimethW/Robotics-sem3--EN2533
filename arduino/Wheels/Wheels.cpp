15f9f47a6661fd383707330bf922a0c8e2f3741d
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-10-05 20:19:57 +0530

    Update and rename wheel.cpp to Wheels.cpp

---
+++ arduino/Wheels/Wheels.cpp
@@ -0,0 +1,40 @@
+/*
+  Wheels.cpp
+  EN2533 - Robot Design and Competition
+  Nilakna Warushavithana, October 2024
+*/
+
+#include "Arduino.h"
+#include "Wheels.h"
+
+Wheel::Wheel(byte controlPinA, byte controlPinB, byte speedPin) {
+    _controlPinA = controlPinA;
+    _controlPinB = controlPinB;
+    _speedPin = speedPin;
+    pinMode(controlPinA, OUTPUT);
+    pinMode(controlPinB, OUTPUT);
+    pinMode(speedPin, OUTPUT);
+}
+
+void Wheel::setSpeed(int speed) {
+    analogWrite(_speedPin, speed);
+}
+
+void Wheel::forward() {
+    digitalWrite(_controlPinA, HIGH);
+    digitalWrite(_controlPinB, LOW);
+}
+
+bool Wheel::backward() {
+    digitalWrite(_controlPinA, LOW);
+    digitalWrite(_controlPinB, HIGH);
+}
+
+void Wheel::stop() {
+    analogWrite(_speedPin, 0);
+}
+
+void Wheel::brake() {
+    digitalWrite(_controlPinA, LOW);
+    digitalWrite(_controlPinA, LOW);
+}
