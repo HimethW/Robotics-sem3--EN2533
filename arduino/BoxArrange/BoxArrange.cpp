a69191e50fd050a5aefcdc1ff906db54e8a67a29
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-10-11 09:01:05 +0530

    Update BoxArrange.cpp

--- arduino/BoxArrange/BoxArrange.cpp
+++ arduino/BoxArrange/BoxArrange.cpp
@@ -13,24 +13,6 @@
 // It is assumed that the robot starts from one of the box positions, facing the direction that the box 
 // would be placed.
 
-void driveTillJunction(Robot *robot, LineFollower *lineFollower) {
-  while (lineFollower->_irSensorArray->junctionMet()) {
-    lineFollower->lineFollow();
-  }
-  robot->brake();
-}
-
-void passNJunctions(Robot *robot, LineFollower *lineFollower, byte numJunctions) {
-  byte junctionsMet = 0;   // Keep moving forward till it passes the relevant number of junctions to reach                              // the end position
-  while (junctionsMet < numJunctions) {
-    lineFollower->lineFollow();
-    if (lineFollower->_irSensorArray->junctionMet()) {
-      junctionsMet++;
-    }
-  }
-  robot->brake();
-}
-
 void boxToTemp(Robot *robot, LineFollower *lineFollower, byte startPos, byte endPos) {
   robot->turn(0, 180);  // Turn 180 degrees.
                         // Maybe create a different function where it uses the IR sensors and the line to
