8320c19cf6ea2e847313807bd0515d2d611d04e7
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-12-01 20:07:40 +0530

    Update IRSensors.h

--- arduino/IRSensors/IRSensors.h
+++ arduino/IRSensors/IRSensors.h
@@ -14,19 +14,38 @@
 #define RED_LINE_BLK_BG 2
 #define BLU_LINE_BLK_BG 3
 
+// we could probably ignore the digital pins on the sensor array and always read only the analog pins.
+// the code can be made to generate a digital output if necessary then. we check if the analog
+// reading is within some threshold, and if so, generate a 1. otherwise generate a zero.
+// some "modes" are defined above, and we can make it so that the digital output is 1 when the
+// color corresponding to the line is detected.
+
 class IRSensorArray {
     public:
-        IRSensorArray(byte *pins, byte numPins);
-        void setMode(byte mode);
+        IRSensorArray(byte *pins, byte numPins, bool invert = false);
+        // if invert is set to true, invert the value read from digitalpin before proceeding further.
+        // this is not really relevant in the analog -> digital situation. corresponds to a within = false 
+        // situation. now that i think of it, maybe the inversion thing is nicer than the within = true/false?
+        // anyway currently im not gonna put the effect of invert into the analog one.
+        void setDigitalMode(byte mode);
+        void setSensorThresholds(byte mode, float lowerThresh, float upperThresh, bool within = true);
+        void setSensorThresholds(byte mode, float* lowerThresh, float* upperThresh, bool within = true);
         void setWeights(float *weightsArray);
         float getWeightedAnalogReading();
         float getWeightedDigitalReading();
-        bool junctionMet();
+        float getWeightedDigitalReading1();
+        void getAnalogReadingsArray(float *targetArray);
+        void getDigitalReadingsArray(byte *targetArray);
+        bool patternDetected(byte *patternArray);
 
     private:
+        byte _digitalMode;
+        byte _invert;
         byte *_pins;
         byte _numPins;
         float *_weights;
+        float (*_upperThresholds)[4]; // 4 modess
+        float (*_lowerThresholds)[4];
 };
 
 #endif
