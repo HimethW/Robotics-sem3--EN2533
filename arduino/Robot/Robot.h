91b6b8c16ea256ba864bb80a270517da33613d5a
Pulasthi Udugamasooriya <55448933+GoldenSoldier@users.noreply.github.com>
2024-12-08 00:22:41 +0530

    Update Robot.h

--- arduino/Robot/Robot.h
+++ arduino/Robot/Robot.h
@@ -9,6 +9,8 @@
 
 #include "Arduino.h"
 #include "Wheels.h"
+#include "IRSensors.h"
+#include "PIDController.h"
 
 #define FORWARD 0
 #define BACKWARD 1
@@ -16,25 +18,31 @@
 class Robot {
   public:
     Robot();
-    float spd;
+    float rightBaseSpeed, leftBaseSpeed; // why are these public? i probably needed to access these from outside...idr
+    PIDController *pidController; // should this be public also?
 
-    void attachWheels(Wheel_t *leftWheels, byte numLeftWheels, Wheel_t *rightWheels, byte numRightWheels);
+    void attachWheels(Wheel *leftWheels, byte numLeftWheels, Wheel *rightWheels, byte numRightWheels);
+    void attachWheels(Wheel *leftWheel, Wheel *rightWheel);
 
-    void setBaseSpeed(float speed);
+    void initPIDController(float K_P, float K_I, float K_D);
+
+    void setBaseSpeed(float right, float left);
     void setRightSpeed(float speed);
     void setLeftSpeed(float speed);
+    void setMinMaxSpeeds(float minSpeed, float maxSpeed);
     
+    void drivePID(float error);
     void drive(byte direction);
     void driveDistance(byte direction, int distance);
     void brake();
     void turn(int radius, int angle); // int or float? will it even be possible to get it to turn with a given radius?
-    void driveTillJunction(LineFollower *lineFollower);
-    void passNJunctions(LineFollower *lineFollower, byte numJunctions);
 
   private:
     Wheel *_leftWheels;
     Wheel *_rightWheels;
     byte _numLeftWheels, _numRightWheels;
+    int _minSpeed, _maxSpeed;
+    byte _driveDirection;
 };
 
 #endif
