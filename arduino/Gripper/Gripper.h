83d5cada3e262a0d76f64a989583402598ce4bae
AYAttanayake <109672195+AYAttanayake@users.noreply.github.com>
2024-12-12 12:19:13 +0530

    Update Gripper.h

--- arduino/Gripper/Gripper.h
+++ arduino/Gripper/Gripper.h
@@ -8,14 +8,17 @@ class Gripper {
     public:
     Gripper (byte controlPinA, byte controlPinB);
 
-    void lift (byte stopAngle); //For 0 < angles <= 90
-    void lower (byte stopAngle); //For 0 < angles <= 90
+    void initialize();
+    void lift (byte stopAngle); //For 0 < angle <= 90
+    void lower ();
     void grab ();
     void release ();
+    void push();
 
     private:
     Servo _servo1;
     Servo _servo2;
+    byte _pinA, _pinB;
 };
 
 #endif
